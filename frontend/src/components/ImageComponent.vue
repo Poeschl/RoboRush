5cc161d74b551c473773985e3c7b48b0aec0b450
Markus Pöschl <Poeschl@users.noreply.github.com>
2023-12-25 16:40:47 +0100

    :memo: Add help text to the How-to page

---
+++ frontend/src/components/ImageComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <img class="grey-shadow" :src="imageSrc" :alt="title" :title="title" />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  imageSrc: string;
+}>();
+</script>
+
+<style scoped lang="scss"></style>
