72f99150ffdf03254e16b54209e1fc2bb0c3431f
Markus Pöschl <Poeschl@users.noreply.github.com>
2024-01-07 18:41:00 +0100

    :recycle: Change the way the api is set to public

--- backend/src/main/kotlin/xyz/poeschl/pathseeker/configuration/OpenApiConfig.kt
+++ backend/src/main/kotlin/xyz/poeschl/pathseeker/configuration/OpenApiConfig.kt
@@ -1,5 +1,6 @@
 package xyz.poeschl.pathseeker.configuration
 
+import io.swagger.v3.oas.models.Operation
 import org.springdoc.core.customizers.OpenApiCustomizer
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
@@ -8,6 +9,11 @@ import org.springframework.context.annotation.Profile
 
 @Configuration
 class OpenApiConfig {
+
+  companion object {
+    public const val VISIBILITY_KEY = "visibility"
+    public const val VISIBILITY_PUBLIC = "public"
+  }
 
   @Bean
   fun robotOpenApi(): GroupedOpenApi = GroupedOpenApi.builder().group("public").addOpenApiCustomizer(filterForPublicTag()).build()
@@ -19,14 +25,25 @@ class OpenApiConfig {
   private fun filterForPublicTag(): OpenApiCustomizer = OpenApiCustomizer { openApi ->
     openApi.paths.entries
       .removeIf { path ->
-        path.value.readOperations().stream().noneMatch { operation -> operation.tags.contains("public") }
+        path.value.readOperations().stream().noneMatch { operation ->
+          visibleForPublic(operation)
+        }
       }
   }
 
   private fun filterForNonePublicTag(): OpenApiCustomizer = OpenApiCustomizer { openApi ->
     openApi.paths.entries
       .removeIf { path ->
-        path.value.readOperations().stream().anyMatch { operation -> operation.tags.contains("public") }
+        path.value.readOperations().stream().anyMatch { operation -> visibleForPublic(operation) }
       }
+  }
+
+  private fun visibleForPublic(operation: Operation): Boolean {
+    return if (!operation.extensions.isNullOrEmpty() && operation.extensions.containsKey("x-$VISIBILITY_KEY")) {
+      val visibility = operation.extensions["x-$VISIBILITY_KEY"] as HashMap<*, *>
+      visibility[VISIBILITY_KEY] == VISIBILITY_PUBLIC
+    } else {
+      false
+    }
   }
 }
