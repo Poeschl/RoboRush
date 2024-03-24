b1b9f20974a92fdd0daa93b77d72038a547075eb
Markus Pöschl <Poeschl@users.noreply.github.com>
2024-03-24 14:37:58 +0100

    :recycle: Combine migration scripts

--- backend/src/main/resources/db/migration/V1_002__connect_robot_and_user.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE robots
-  ADD user_id BIGINT NOT NULL;
-
-ALTER TABLE robots
-  ADD CONSTRAINT user_id
-    FOREIGN KEY (user_id) REFERENCES users;
-
-CREATE INDEX index_robots_user_id ON robots (user_id);
