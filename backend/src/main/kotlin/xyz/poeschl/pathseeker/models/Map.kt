952cbdb68136ca0b0bb24a7b7616e5e0dce41ebe
Markus Pöschl <Poeschl@users.noreply.github.com>
2024-03-31 17:34:31 +0200

    :sparkles: Add tile type for marking of start and end tiles

--- backend/src/main/kotlin/xyz/poeschl/pathseeker/models/Map.kt
+++ backend/src/main/kotlin/xyz/poeschl/pathseeker/models/Map.kt
@@ -5,7 +5,7 @@ import xyz.poeschl.pathseeker.configuration.Builder
 import kotlin.math.abs
 
 @GeneratePojoBuilder(withBuilderInterface = Builder::class)
-data class Map(val size: Size, val mapData: Array<Array<Tile>>, val possibleStartPositions: List<Position>) {
+data class Map(val size: Size, val mapData: Array<Array<Tile>>, val possibleStartPositions: List<Position>, val targetPosition: Position) {
   override fun equals(other: Any?): Boolean {
     if (this === other) return true
     if (javaClass != other?.javaClass) return false
@@ -15,6 +15,7 @@ data class Map(val size: Size, val mapData: Array<Array<Tile>>, val possibleStar
     if (size != other.size) return false
     if (!mapData.contentDeepEquals(other.mapData)) return false
     if (possibleStartPositions != other.possibleStartPositions) return false
+    if (targetPosition != other.targetPosition) return false
 
     return true
   }
@@ -23,12 +24,13 @@ data class Map(val size: Size, val mapData: Array<Array<Tile>>, val possibleStar
     var result = size.hashCode()
     result = 31 * result + mapData.contentDeepHashCode()
     result = 31 * result + possibleStartPositions.hashCode()
+    result = 31 * result + targetPosition.hashCode()
     return result
   }
 }
 
 @GeneratePojoBuilder(withBuilderInterface = Builder::class)
-data class Tile(val position: Position, val height: Int = 0)
+data class Tile(val position: Position, val height: Int = 0, val type: TileType = TileType.DEFAULT_TILE)
 
 @GeneratePojoBuilder(withBuilderInterface = Builder::class)
 data class Position(val x: Int, val y: Int) {
@@ -51,6 +53,12 @@ data class Position(val x: Int, val y: Int) {
   fun getDistanceTo(other: Position): Int {
     return abs(this.x - other.x) + abs(this.y - other.y)
   }
+}
+
+enum class TileType {
+  DEFAULT_TILE,
+  START_TILE,
+  TARGET_TILE
 }
 
 @GeneratePojoBuilder(withBuilderInterface = Builder::class)
