cf01faa8fb1526e2169ee1736560699e95ec67c5
Markus Pöschl <Poeschl@users.noreply.github.com>
2025-08-28 21:41:59 +0200

    :feature: Enhance development environment scripts to manage Podman and Docker machines

--- start_dev_env.bat
+++ start_dev_env.bat
@@ -1,26 +1,53 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Check if podman-compose is available
+:: Check if podman machine is available and not already running
+where podman > nul 2>&1
+if %errorlevel% equ 0 (
+    for /f "tokens=*" %%i in ('podman machine inspect') do (
+        echo %%i | findstr /i /c:"\"State\": \"running\"" > nul
+        if !errorlevel! neq 0 (
+            echo Podman machine detected but not running, starting machine...
+            podman machine start
+            goto :continue
+        ) else (
+            echo Podman machine is already running.
+            goto :continue
+        )
+    )
+)
+
+:continue
+:: Check if docker machine is available and not already running
+where docker-machine > nul 2>&1
+if %errorlevel% equ 0 (
+    docker-machine ls --filter "STATE=Running" | findstr /i "Running" > nul
+    if %errorlevel% neq 0 (
+        echo Docker machine detected but not running, starting the default machine...
+        docker-machine start default
+        :: Set Docker environment variables
+        for /f "tokens=*" %%i in ('docker-machine env --shell cmd default') do %%i
+    ) else (
+        echo Docker machine is already running.
+    )
+)
+
+:: Determine the appropriate compose command
 where podman-compose > nul 2>&1
 if %errorlevel% equ 0 (
     set "exec=podman-compose"
 ) else (
-    :: Check if docker-compose is available
     where docker-compose > nul 2>&1
     if %errorlevel% equ 0 (
         set "exec=docker-compose"
     ) else (
-        :: If neither is available, assume docker compose
         set "exec=docker compose"
     )
 )
 
-:: Set hostname
 set "HOSTNAME=%COMPUTERNAME%.local"
 
 echo Container startup
-:: Execute the selected command
 call %exec% up %*
 
 endlocal
