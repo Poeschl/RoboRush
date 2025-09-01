ce20bbc45d00b35e54620047807e4d0f754e4b70
Markus Pöschl <Poeschl@users.noreply.github.com>
2025-09-01 20:26:53 +0200

    :recycle:  Update map data structure to use TileDTO for improved type handling

--- backend/src/main/kotlin/xyz/poeschl/roborush/controller/restmodels/MapDtos.kt
+++ backend/src/main/kotlin/xyz/poeschl/roborush/controller/restmodels/MapDtos.kt
@@ -4,8 +4,8 @@ import net.karneim.pojobuilder.GeneratePojoBuilder
 import xyz.poeschl.roborush.configuration.Builder
 import xyz.poeschl.roborush.models.Position
 import xyz.poeschl.roborush.models.Size
+import xyz.poeschl.roborush.models.TileType
 import xyz.poeschl.roborush.repositories.Map
-import xyz.poeschl.roborush.repositories.Tile
 
 data class MapGenerationResult(val warnings: List<String>)
 
@@ -13,6 +13,12 @@ data class MapActiveDto(val active: Boolean)
 
 @GeneratePojoBuilder(withBuilderInterface = Builder::class)
 data class MapAttributeSaveDto(val mapName: String, val maxRobotFuel: Int, val solarChargeRate: Double)
+
+data class TileDTO(
+  val position: Position,
+  val height: Int = 0,
+  val type: TileType = TileType.DEFAULT_TILE
+)
 
 data class PlaygroundMap(
   val id: Long,
@@ -23,7 +29,7 @@ data class PlaygroundMap(
   val maxRobotFuel: Int = 300,
   val solarChargeRate: Double = 0.0,
   val active: Boolean = false,
-  val mapData: List<Tile> = mutableListOf(),
+  val mapData: List<TileDTO> = mutableListOf(),
   val minHeight: Int,
   val maxHeight: Int
 ) {
@@ -37,7 +43,7 @@ data class PlaygroundMap(
     map.maxRobotFuel,
     map.solarChargeRate,
     map.active,
-    map.mapData,
+    map.mapData.map { TileDTO(it.position, it.height, it.type) },
     minMax.first,
     minMax.second
   )
